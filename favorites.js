4afb76256d23c32c5076e527ca872102e88a77b6
hontonoran <57138301+hontonoran@users.noreply.github.com>
2026-07-03 14:39:53 +0000

    Deploying to gh-pages from @ yevhennikulin/advanced-java-group-2@74b480c857f54a3eea7b464d488b3ca1fbc082cc 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,2 @@
-import"./assets/scroll-up-CsqlUVtz.js";import"./assets/vendor-DdHXTJIf.js";console.log("Your Energy app initialized");
+import"./assets/scroll-up-DylexGML.js";import"./assets/vendor-DdHXTJIf.js";console.log("Your Energy app initialized");
 //# sourceMappingURL=favorites.js.map
